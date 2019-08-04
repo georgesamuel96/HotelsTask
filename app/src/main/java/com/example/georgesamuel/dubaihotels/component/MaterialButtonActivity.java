be4c243235f70890bf4f41dc006d30fad7865313
AlShaymaa1 <shaymaa908@yahoo.com>
2019-08-04 12:27:32 +0200

    material button

---
+++ app/src/main/java/com/example/georgesamuel/dubaihotels/component/MaterialButtonActivity.java
@@ -0,0 +1,33 @@
+package com.example.georgesamuel.dubaihotels.component;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.georgesamuel.dubaihotels.R;
+import com.google.android.material.button.MaterialButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MaterialButtonActivity extends AppCompatActivity {
+
+    @BindView(R.id.button_open_collapse)
+    MaterialButton btnOpenCollapse;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_material_button);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.button_open_collapse)
+    public void openCollapseScreen(View view){
+        Intent intent=new Intent(MaterialButtonActivity.this,MaterialComponentActivity.class);
+        startActivity(intent);
+    }
+}
